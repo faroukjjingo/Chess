19f3cb83034056644cb63245914024af0e245ec0
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-14 00:07:27 +0300

    Update route.ts

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,5 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import GitHubProvider from 'next-auth/providers/github';
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }: any) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-};
+import { authOptions } from '@/lib/auth';
 
 const handler = NextAuth(authOptions);
 

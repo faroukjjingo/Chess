88850113e1d0e01cf0af5a0b5b39cdfa2a1a01bf
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-14 00:13:43 +0300

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
-import { SessionProvider } from 'next-auth/react';
 import { Inter } from 'next/font/google';
+import AuthProvider from '../components/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider>{children}</SessionProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
